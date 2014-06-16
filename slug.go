a9d8e2353f2f690af47183fb61d80625edf066ad
peferron <pe.ferron@gmail.com>
2014-06-16 11:30:54 +0800

    Initial commit

---
+++ slug.go
@@ -0,0 +1,50 @@
+// Package slug provides utilities for generating typical URL slugs.
+package slug
+
+import (
+	"strings"
+	"unicode"
+
+	"github.com/fiam/gounidecode/unidecode"
+)
+
+// Slug returns a slug generated from s, with the maximum length l and the separator sep.
+func Slug(s string, l int, sep string) string {
+	decoded := unidecode.Unidecode(s)
+	slug := ""
+
+	for _, d := range decoded {
+		r := convert(d, sep)
+
+		switch r {
+		case "":
+			continue
+		case sep:
+			if len(slug) > 0 && !strings.HasSuffix(slug, sep) {
+				slug += sep
+			}
+		default:
+			slug += string(r)
+		}
+
+		if len(slug) >= l {
+			break
+		}
+	}
+
+	return strings.TrimSuffix(slug, sep)
+}
+
+// convert returns the slug representation of r, using the separator sep.
+func convert(r rune, sep string) string {
+	switch {
+	case 'a' <= r && r <= 'z' || '0' <= r && r <= '9':
+		return string(r)
+	case 'A' <= r && r <= 'Z':
+		return string(unicode.ToLower(r))
+	case r == ' ' || r == ',' || r == '.' || r == '/' || r == '\\' || r == '-' || r == '_' ||
+		r == '=':
+		return sep
+	}
+	return ""
+}
---
+++ slug_test.go
@@ -0,0 +1,41 @@
+package slug
+
+import (
+	"testing"
+)
+
+var (
+	tests = []struct {
+		iS   string
+		iL   int
+		iSep string
+		oS   string
+	}{
+		{"", 50, "-", ""},
+		{" ", 50, "-", ""},
+		{"a lovely day", 50, "-", "a-lovely-day"},
+		{"a lovely day", 50, "123", "a123lovely123day"},
+		{"A Lovely Day", 50, "-", "a-lovely-day"},
+		{"a lovely day", 10, "-", "a-lovely-d"},
+		{"a lovely day", 9, "-", "a-lovely"},
+		{"a lovely day", 8, "-", "a-lovely"},
+		{"wo ai 北京", 50, "-", "wo-ai-bei-jing"},
+		{"wo ai 北京", 12, "-", "wo-ai-bei-ji"},
+		{"wo ai 北京", 10, "-", "wo-ai-bei"},
+		{"wo ai 北京", 10, "-", "wo-ai-bei"},
+		{"wo ai 北京", 7, "-", "wo-ai-b"},
+		{"wo ai 北京", 1, "-", "w"},
+		{"-a -- b__c", 50, "-", "a-b-c"},
+	}
+)
+
+func TestSlug(t *testing.T) {
+	for _, test := range tests {
+		if out := Slug(test.iS, test.iL, test.iSep); out != test.oS {
+			t.Errorf("For input string '%s' and maximum length %d and separator '%s', "+
+				"expected output '%s' (len %d), was '%s' (len %d)",
+				test.iS, test.iL, test.iSep,
+				test.oS, len(test.oS), out, len(out))
+		}
+	}
+}
